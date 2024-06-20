72e3708007a9791cf86c8e03e2e3e9cf47d8b57a
Camilo Echeverri <camilo.echeverri.100@gmail.com>
2024-06-19 20:01:01 -0500

    Added Google fonds for the text style. Corrected JS script
    minor corrections on the margins of the buttons

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 addEventListener('DOMContentLoaded', () => {
 
-    let quoteTags = document.querySelectorAll('.quote-tags');
-    let quoteText = document.getElementById('.quote-text');
-    let copyQuote = document.getElementById('.copy-quote');
-    let newQuote = document.getElementById('.new-quote');
-
+    let quoteTags = document.querySelector('.quote-tags');
+    let quoteText = document.getElementById('quote-text');
+    let copyQuote = document.getElementById('copy-quote');
+    let newQuote = document.getElementById('new-quote');
+    let quoteAutor = document.getElementById('quote-Autor');
 
     async function getQuote() {
         fetch('https://api.quotable.io/random')
             .then(response => response.json())
             .then(data => {
-                return data;
+                quoteText.textContent = '"'+data.content+'"';
+                quoteAutor.textContent = data.author;
+                
+                quoteTags.innerHTML = '';
+                data.tags.forEach(tag => {
+                    let tagElement = document.createElement('span');
+                    tagElement.classList.add('tag');
+                    tagElement.textContent = tag;
+                    quoteTags.appendChild(tagElement);
+                });
             });
     };
 
-    async function changeQuote(){
-        data = await getQuote();
-        quoteText.textContent = data.content;
-        quoteTags.textContent = data.tags;
-    };
-
-    newQuote.addEventListener('click', changeQuote);
+    if (getQuote){
+        newQuote.addEventListener('click', getQuote);
+    }
 
     copyQuote.addEventListener('click', () => {
         let text = quoteText.textContent;
         navigator.clipboard.writeText(text);
     });
+
+    getQuote();
 
 });
