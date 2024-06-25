80b84cdaba2441fb86c8afd35ba1a62551f0a322
Camilo Echeverri <camilo.echeverri.100@gmail.com>
2024-06-24 21:25:15 -0500

    Add. two new API quote resources FavQS and Zenquotes
    Minor changes to the autor name font-size

--- script.js
+++ script.js
@@ -5,14 +5,26 @@ addEventListener('DOMContentLoaded', () => {
     let copyQuote = document.getElementById('copy-quote');
     let newQuote = document.getElementById('new-quote');
     let quoteAutor = document.getElementById('quote-Autor');
+    let apiQuotable = document.getElementById('api-quotable');
+    let apiZen = document.getElementById('api-zen');
+    let apiFavqs = document.getElementById('api-favqs');
 
-    async function getQuote() {
-        fetch('https://api.quotable.io/random')
-            .then(response => response.json())
-            .then(data => {
-                quoteText.textContent = '"'+data.content+'"';
+
+    const API_QUOTABLE = 'https://api.quotable.io/random'
+    const API_ZEN = 'https://zenquotes.io/api/random/'
+    const API_FAVQS = 'https://favqs.com/api/qotd'
+    let currentApi = "" 
+
+    async function getQuote(quoteSource) {
+        try {
+            const response = await fetch(quoteSource);
+            const data = await response.json();
+
+            if (quoteSource === API_QUOTABLE) {
+                currentApi = API_QUOTABLE
+                quoteText.textContent = '"' + data.content + '"';
                 quoteAutor.textContent = data.author;
-                
+
                 quoteTags.innerHTML = '';
                 data.tags.forEach(tag => {
                     let tagElement = document.createElement('span');
@@ -20,18 +32,65 @@ addEventListener('DOMContentLoaded', () => {
                     tagElement.textContent = tag;
                     quoteTags.appendChild(tagElement);
                 });
-            });
+            }
+            else if (quoteSource === API_ZEN) {
+                currentApi = API_ZEN
+                quoteText.textContent = '"' + data[0].q + '"';
+                quoteAutor.textContent = data[0].a;
+                tagElement = document.createElement('span');
+                tagElement.classList.add('tag');
+                tagElement.textContent = 'no tags available';
+                quoteTags.appendChild(tagElement);
+
+            }
+            else if (quoteSource === API_FAVQS) {
+                currentApi = API_FAVQS
+                quoteText.textContent = '"' + data.quote.body + '"';
+                quoteAutor.textContent = data.quote.author;
+                quoteTags.innerHTML = 'No tags available';
+                if (data.quote.tags.length) {
+                    quoteTags.innerHTML = '';
+                    data.quote.tags.forEach(tag => {
+                        let tagElement = document.createElement('span');
+                        tagElement.classList.add('tag');
+                        tagElement.textContent = tag;
+                        quoteTags.appendChild(tagElement);
+                    });
+                }
+            }
+            else {
+                quoteText.textContent = 'Error';
+            }
+
+        } catch (error) {
+            quoteText.textContent = 'Error fetching quote';
+            console.error('Error:', error);
+        }
+  
     };
 
-    if (getQuote){
-        newQuote.addEventListener('click', getQuote);
-    }
 
-    copyQuote.addEventListener('click', () => {
-        let text = quoteText.textContent;
-        navigator.clipboard.writeText(text);
-    });
+newQuote.addEventListener('click', () => getQuote(currentApi? currentApi : API_QUOTABLE));
 
-    getQuote();
+copyQuote.addEventListener('click', () => {
+    let text = quoteText.textContent;
+    navigator.clipboard.writeText(text);
+});
+
+apiQuotable.addEventListener('click', () => {
+    getQuote(API_QUOTABLE);
+});
+
+apiZen.addEventListener('click', () => {
+    getQuote(API_ZEN);
+});
+
+apiFavqs.addEventListener('click', () => {
+    getQuote(API_FAVQS);
+
+});
+
+getQuote(API_QUOTABLE);
+
 
 });
